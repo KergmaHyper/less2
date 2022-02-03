b37451fcf1af467789cacd363227869364b8d02f
kergma@gmail.com <kergma@gmail.com>
2022-02-03 13:57:37 +0200

    fix task3

--- less/src/Snail.java
+++ less/src/Snail.java
@@ -8,20 +8,24 @@ public class Snail {
         int b = scanner.nextInt();
         int h = scanner.nextInt();
         scanner.close();
-        if (a > b) {
-            int hDay = 0;
-            int countDays = 0;
 
-            do {
-                if (countDays > 0)
-                    hDay -= b;
-                countDays++;
-                hDay += a;
-            } while (hDay < h);
+        int hDay = 0; // первоначальная высота куда доползла
+        int countDays = 0;// количество дней
 
+        do {
+            if (countDays > 0)// если не первый день вычесть высоту на которую сползла
+                hDay -= b;
+            if (hDay <= 0 & countDays > 0)// если за день остались там же или ниже - прекратиь цикл
+                break;
+            countDays++;// прибавить день
+
+            hDay += a;// прибавить высоту
+        } while (hDay < h);// если за день не доползли до вершины, еще на цикл
+
+        if (hDay > 0)
             System.out.println(countDays);
-        } else {
+        else
             System.out.println("Impossible");
-        }
+
     }
 }
