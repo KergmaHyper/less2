d84c49310d285569bc1977558493fab82457b19a
kergma@gmail.com <kergma@gmail.com>
2022-02-03 16:32:25 +0200

    task8 and shange SDK to oracle JDK 17

---
+++ less/src/QuadraticEquation.java
@@ -0,0 +1,26 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        DecimalFormat numFormat = new DecimalFormat("#.##");
+        Scanner scanner = new Scanner(" 1, -2, 1");
+        float a = scanner.nextFloat();
+        float b = scanner.nextFloat();
+        float c = scanner.nextFloat();
+        scanner.close();
+
+        float D = b * b - 4 * a * c;
+        if (D < 0) {
+            System.out.println("no roots");
+        } else if (D == 0) {
+            float x = -b / 2 / a;
+            System.out.printf("%s\n", numFormat.format(x));
+        } else {
+            double x1 = ((-b - Math.sqrt(D)) / 2 / a);
+            double x2 = ((-b + Math.sqrt(D)) / 2 / a);
+            System.out.printf("%s %s\n", numFormat.format(x1), numFormat.format(x2));
+        }
+
+    }
+}
